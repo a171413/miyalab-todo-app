3cd62697bab3ec1b19cd7d86ae1f2b745ad2062e
a171413 <a171413g@st.u-gakugei.ac.jp>
2020-08-20 11:55:45 +0900

    テンプレート作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,71 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        miyalab-todo-app
-      </h1>
-      <h2 class="subtitle">
-        My impressive Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <section class="container">
+    <h1>Todoリスト</h1>
+    <div class="addArea">
+      <input id="addName" type="text" name="addName" placeholder="タスクを入力してください">
+      <button id="addButton" class="button button--green">
+        追加
+      </button>
     </div>
-  </div>
+    <div class="Filter">
+      <button class="button button--gray is-active">
+        全て
+      </button>
+      <button class="button button--gray">
+        作業前
+      </button>
+      <button class="button button--gray">
+        作業中
+      </button>
+      <button class="button button--gray">
+        完了
+      </button>
+    </div>
+    <table class="Lists">
+      <thread>
+        <tr>
+          <th>タスク</th>
+          <th>登録日時</th>
+          <th>状態</th>
+          <th />
+        </tr>
+      </thread>
+      <tbody>
+        <!--v-forで繰り返し-->
+        <tr v-for="(item, index) in todos" :key="index">
+          <td>
+            {{ item.content }}
+          </td>
+          <td>
+            {{ item.created }}
+          </td>
+          <td>
+            <button class="button">
+              {{ item.state }}
+            </button>
+          </td>
+          <td>
+            <button class="button button--delete">
+              削除
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import { mapState } from 'vuex'
 
 export default {
-  components: {
-    Logo
+  data () {
+    return {
+      content: ''
+    }
+  },
+  computed: {
+    ...mapState(['todos'])
   }
 }
 </script>
@@ -48,25 +80,33 @@ export default {
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.addArea {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 }
+/*.title {*/
+/*  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,*/
+/*    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;*/
+/*  display: block;*/
+/*  font-weight: 300;*/
+/*  font-size: 100px;*/
+/*  color: #35495e;*/
+/*  letter-spacing: 1px;*/
+/*}*/
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+/*.subtitle {*/
+/*  font-weight: 300;*/
+/*  font-size: 42px;*/
+/*  color: #526488;*/
+/*  word-spacing: 5px;*/
+/*  padding-bottom: 15px;*/
+/*}*/
 
-.links {
-  padding-top: 15px;
-}
+/*.links {*/
+/*  padding-top: 15px;*/
+/*}*/
 </style>
