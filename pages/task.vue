c7b57e6ee7259bb9d5221050629ad568ff9545c8
ffWisteria <e175415m@st.u-gakugei.ac.jp>
2020-09-12 14:37:21 +0900

    task error訂正

--- pages/task.vue
+++ pages/task.vue
@@ -1,64 +1,67 @@
 <template>
-    <section class="container">
-        <h1>TaskList</h1>
-        <br>
-        <table>
-        <tr>
-            <th>Task</th>
-            <td>
-                <input type="text" name="task" class="task">
-            </td>
-        </tr>
-        <tr>
-            <th>Date</th>
-            <td>
-                <input type="datetime-local" name="datetime"class="datetime">
-            </td>
-        <tr>
-            <th></th>
-            <td><button @click="insert">save</button>
-        </tr>
-        </table>
+  <section class="container">
+    <h1>TaskList</h1>
+    <br>
+    <table>
+      <tr>
+        <th>Task</th>
+        <td>
+          <input type="text" name="task" class="task">
+        </td>
+      </tr>
+      <tr>
+        <th>Date</th>
+        <td>
+          <input type="datetime-local" name="datetime" class="datetime">
+        </td>
+      </tr>
+      <th>Save</th>
+        <td>
+          <button @click="insert">
+            save
+          </button>
+        </td>
+      </tr>
+    </table>
 
-        <hr>
-        <ul class="list">
-            <li v-for="task in page_tasks">
-                {{task}}
-            </li>
-        </ul>
-        <hr>
-    </section>
+    <hr>
+    <ul class="list">
+      <li v-for="(item, index) in page_tasks" :key="index">
+        {{ item.task }}
+      </li>
+    </ul>
+    <hr>
+  </section>
 </template>
 
 <script>
 export default {
-    data: function(){
-        return {
-            task:'',
-            datetime:''
-        };
+  data: function(){
+    return {
+      task:'',
+      datetime:''
+    };
+  },
+  computed: {
+    task: function(){
+      return this.$store.state.task.task;
     },
-    computed:{
-        task:function(){
-            return this.$store.state.task.task;
-        }
-        page_tasks:function(){
-            var arr=[];
-            var data = this.$store.state.task.task;
-            data.forEach(element => {
-                arr.push(element);
-            });
-            return arr;
-        }
-    },
-    methods:{
-        insert: function(){
-            this.$store.commit('task/insert', 
-                {task:this.task,datetime:this.datetime}); 
-            this.task = '';
-            this.datetime='';
-        }
-    },
+    page_tasks: function(){
+      var arr = [];
+      var data = this.$store.state.task.task;
+      data.forEach(element => {
+        arr.push(element);
+      });
+      return arr;
+    }
+  },
+  methods: {
+    insert: function(){
+      this.$store.commit('task/insert', { task:this.task, datetime:this.datetime }); 
+      this.task = '';
+      this.datetime = '';
+    }
+  },
 }
 </script>
 
@@ -71,22 +74,22 @@ h1 {
   color: #345980;
 }
 button {
-    font-size: 14pt;
-    padding:1px 10px;
-    margin:5px;
+  font-size: 14pt;
+  padding:1px 10px;
+  margin:5px;
 }
 
 th {
-    background-color: #3cb371;
-    color: white;
+  background-color: #3cb371;
+  color: white;
 }
 td {
-    background-color:#fffacd;
-    color:black;
-    padding:5px;
+  background-color:#fffacd;
+  color:black;
+  padding:5px;
 }
 .list {
-    cursor : pointer;
+  cursor : pointer;
 }
 
 </style>
