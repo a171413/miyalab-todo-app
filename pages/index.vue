925c023c79a02e103c59e68a8bc5cacfe15c6878
a171413 <a171413g@st.u-gakugei.ac.jp>
2020-09-26 13:59:00 +0900

    template等変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,73 @@
 <template>
   <section class="container">
     <h1>Todo App</h1>
-    <p>
-      タイトル：
-      <input
-        v-model="board.title"
-        type="text"
-      ></input>
-    </p>
-    <p>
-      ボードの詳細：
-      <input
-        v-model="board.detail"
-        type="text"
-      ></input>
-    </p>
-    <div>
-      <button
-        @click="insertBoard"
+    <el-form
+      ref="board"
+      :model="board"
+      :rules="rules"
+    >
+      <el-form-item
+        label="ボードタイトル"
+        prop="title"
       >
-        ボードを追加する
-      </button>
+        <el-input
+          v-model="board.title"
+          auto-complete="off"
+        />
+      </el-form-item>
+      <el-form-item
+        label="ボード詳細"
+        prop="detail"
+      >
+        <el-input
+          v-model="board.detail"
+          auto-complete="off"
+        />
+      </el-form-item>
+      <el-checkbox
+        v-model="isDeleteMode"
+      >
+        ボードを削除する
+      </el-checkbox>
+      <el-form-item
+        v-show="isDeleteMode"
+        label="削除するボード番号"
+        prop="id"
+      >
+        <el-input
+          v-model="board.id"
+          auto-complete="off"
+        />
+      </el-form-item>
+    </el-form>
+    <!--    <div-->
+    <!--      v-show="isDeleteMode"-->
+    <!--    >-->
+    <!--      <p>-->
+    <!--        削除したいボードの番号-->
+    <!--        <el-input-->
+    <!--          v-model="board.id"-->
+    <!--          placeholder="削除したいボードの番号を入力してください"-->
+    <!--        />-->
+    <!--      </p>-->
+    <!--    </div>-->
+    <div>
+      <el-button
+        type="primary"
+        @click="handleClickSubmit"
+      >
+        {{ buttonText }}
+      </el-button>
     </div>
     <ul>
       <li v-for="(item, index) in $store.state.boards" :key="index">
-        <span>{{ index }}</span> : <span>{{ item.title }}</span>
+        <nuxt-link
+          :to="{ name: 'board-id', params: { id: index } }"
+        >
+          {{ index }} : {{ item.title }}
+        </nuxt-link>
       </li>
     </ul>
-    <!--<checkbox_button v-model="isDeleteMode">選択したボードを削除する</checkbox_button>-->
   </section>
 </template>
 
@@ -36,23 +76,77 @@ export default {
   data () {
     return {
       board: {
+        id: '',
         title: '',
         detail: ''
+      },
+      isDeleteMode: false,
+      rules: {
+        title: [
+          { required: true, message: 'ボードのタイトルを入力してください', trigger: 'blur' },
+          { min: 1, max: 30, message: 'ボードのタイトルは1〜30文字で入力してください', trigger: 'blur' }
+        ],
+        detail: [
+          { required: true, message: 'ボードの詳細を入力してください', trigger: 'blur' }
+        ],
+        id: [
+          { required: true, message: '削除したいボードの番号を入力してください', trigger: 'blur' }
+        ]
       }
-      // isDeleteMode: false
+    }
+  },
+  computed: {
+    buttonText () {
+      return this.isDeleteMode ? '削除' : '追加'
     }
   },
   methods: {
-    insertBoard () {
-      this.$store.commit('insertBoard', { title: this.board.title, detail: this.board.detail })
+    handleClickSubmit () {
+      if (this.isDeleteMode) { // deleteMode=true=削除したいとき
+        try {
+          this.$store.commit('removeBoard', this.board.id)
+          this.$notify.success({
+            title: 'ボードの削除成功',
+            message: 'ボードの削除に成功しました',
+            position: 'bottom-right',
+            duration: 3000
+          })
+          this.board.title = ''
+          this.board.detail = ''
+          this.board.id = ''
+        } catch (e) {
+          this.$notify.error({
+            title: 'ボードの削除失敗',
+            message: '検索に失敗し，既に削除されているかそのタイトルのボードは存在しません',
+            position: 'bottom-right',
+            duration: 3000
+          })
+        }
+      } else { // deleteMode=false=追加したいとき
+        try {
+          this.$store.commit('insertBoard', { id: this.$store.state.boards.length, title: this.board.title, detail: this.board.detail })
+          this.$notify.success({
+            title: 'ボードの作成完了',
+            message: 'ボードの作成に成功しました',
+            position: 'bottom-right',
+            duration: 3000
+          })
+          this.board.title = ''
+          this.board.detail = ''
+        } catch (e) {
+          this.$notify.error({
+            title: 'ボードの作成失敗',
+            message: 'ボードの作成に失敗しました',
+            position: 'bottom-right',
+            duration: 3000
+          })
+        }
+      }
     }
-    // removeBoard () {
-    //   this.$store.commit('removeBoard', )
-    // }
   }
 }
 </script>
 
-<style scoped>
+<style >
 
 </style>
