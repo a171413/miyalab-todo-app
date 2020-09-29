22b0ee25a07713ad24095f52c104854b812ba0ad
a171413 <a171413g@st.u-gakugei.ac.jp>
2020-09-29 13:51:46 +0900

    動的ルーティングをできるように

--- pages/board/_id.vue
+++ pages/board/_id.vue
@@ -1,80 +1,47 @@
-<!--<template>-->
-<!--  <section class="container">-->
-<!--    <h1>Todoリスト</h1>-->
-<!--    <div class="addArea">-->
-<!--      <input id="addName" type="text" name="addName" placeholder="タスクを入力してください">-->
-<!--      <button id="addButton" class="button button&#45;&#45;green">-->
-<!--        追加-->
-<!--      </button>-->
-<!--    </div>-->
-<!--    <div class="Filter">-->
-<!--      <button class="button button&#45;&#45;gray is-active">-->
-<!--        全て-->
-<!--      </button>-->
-<!--      <button class="button button&#45;&#45;gray">-->
-<!--        作業前-->
-<!--      </button>-->
-<!--      <button class="button button&#45;&#45;gray">-->
-<!--        作業中-->
-<!--      </button>-->
-<!--      <button class="button button&#45;&#45;gray">-->
-<!--        完了-->
-<!--      </button>-->
-<!--    </div>-->
-<!--    <table class="Lists">-->
-<!--      <thread>-->
-<!--        <tr>-->
-<!--          <th>タスク</th>-->
-<!--          <th>登録日時</th>-->
-<!--          <th>状態</th>-->
-<!--          <th />-->
-<!--        </tr>-->
-<!--      </thread>-->
-<!--      <tbody>-->
-<!--        &lt;!&ndash;v-forで繰り返し&ndash;&gt;-->
-<!--        <tr v-for="(item, index) in todos" :key="_id">-->
-<!--          <td>-->
-<!--            {{ item.content }}-->
-<!--          </td>-->
-<!--          <td>-->
-<!--            {{ item.created }}-->
-<!--          </td>-->
-<!--          <td>-->
-<!--            <button class="button">-->
-<!--              {{ item.state }}-->
-<!--            </button>-->
-<!--          </td>-->
-<!--          <td>-->
-<!--            <button class="button button&#45;&#45;delete">-->
-<!--              削除-->
-<!--            </button>-->
-<!--          </td>-->
-<!--        </tr>-->
-<!--      </tbody>-->
-<!--    </table>-->
-<!--  </section>-->
-<!--</template>-->
+<template>
+  <section class="container">
+    <h1>ボードID＝{{ $route.params.id }}のページ</h1>
+  </section>
+</template>
 
-<!--<script>-->
-
-<!--</script>-->
-
-<!--<style>-->
-<!--  .container {-->
-<!--    margin: 0 auto;-->
-<!--    min-height: 100vh;-->
-<!--    display: flex;-->
-<!--    justify-content: center;-->
-<!--    align-items: center;-->
-<!--    text-align: center;-->
-<!--  }-->
-
-<!--  .addArea {-->
-<!--    margin: 0 auto;-->
-<!--    min-height: 100vh;-->
-<!--    display: flex;-->
-<!--    justify-content: center;-->
-<!--    align-items: center;-->
-<!--    text-align: center;-->
-<!--  }-->
-<!--</style>-->
+<script>
+export default {
+  date () {
+    return {
+      task: {
+        id: '',
+        title: '',
+        detail: ''
+      },
+      isDeleteMode: false
+    }
+  },
+  computed: {
+    buttonText () {
+      return this.isDeleteMode ? '削除' : '追加'
+    }
+  },
+  methods: {
+    handleClickSubmit () {
+      if (this.isDeleteMode) {
+        try {
+          this.$store.task.commit('removeTask', this.task.id)
+          this.$notify.success({
+            title: 'タスクの削除成功',
+            message: 'タスクの削除に成功しました',
+            position: 'bottom-right',
+            duration: 3000
+          })
+        } catch (e) {
+          this.$notify.error({
+            title: 'タスクの削除失敗',
+            message: '検索に失敗し，既に削除されているかそのタイトルのタスクは存在しません',
+            position: 'bottom-right',
+            duration: 3000
+          })
+        }
+      }
+    }
+  }
+}
+</script>
